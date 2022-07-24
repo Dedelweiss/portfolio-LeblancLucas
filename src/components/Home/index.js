ca337fe26d8a86506f0b5e3827923d5aece9f79d
Dedelweiss <leblanc.lucas29@gmail.com>
2022-07-24 18:14:59 +0200

    Language

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,40 +4,34 @@ import ImageHome from '../../assets/images/profilImgHome.jpg'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next'
 
 const Home = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation()
   const [letterClass, setLetterClass] = useState('text-animate')
-  const helloArray = 'Hello,'.split('')
-  const nameArray =  t("i_am").split('')
-  const jobArray = 'web developer'.split('')
+  const helloArray = t('hello').split('')
+  const nameArray = t('i_am').split('')
+  const jobArray = t('web_developer').split('')
 
   useEffect(() => {
-    async function changeClass(){
+    async function changeClass() {
       await setTimeout(() => {
         setLetterClass('text-animate-hover')
-      }, 4000) 
+      }, 4000)
     }
     changeClass()
   }, [])
-
-  const lngs = {
-    fr: { nativeName: 'Français' },
-    en: { nativeName: 'English' }
-  };
 
   return (
     <>
       <div className="container home-page">
         <div className="text-zone">
-        {Object.keys(lngs).map((lng) => (
-            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
-              {lngs[lng].nativeName}
-            </button>
-          ))}
           <h1>
-            <AnimatedLetters letterClass={letterClass} strArray={helloArray} idx={6}></AnimatedLetters>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={helloArray}
+              idx={6}
+            ></AnimatedLetters>
             <br />
             <AnimatedLetters
               letterClass={letterClass}
@@ -51,16 +45,16 @@ const Home = () => {
               idx={23}
             ></AnimatedLetters>
           </h1>
-          <h2>Full stack web developer, specialized in Front end Test</h2>
+          <h2>{t('full_stack')}</h2>
           <Link to="/contact" className="flat-button">
-            CONTACT ME
+            {t('contact_me')}
           </Link>
         </div>
         <div className="image-zone">
           <img src={ImageHome} alt="Lucas profile" className="profile-img" />
         </div>
       </div>
-      <Loader type='line-scale-pulse-out-rapid' />
+      <Loader type="line-scale-pulse-out-rapid" />
     </>
   )
 }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,55 +4,84 @@ import LogoL from '../../assets/images/esr.webp'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useTranslation } from 'react-i18next'
 
-const Sidebar = () => (
-  <div className="nav-bar">
-    <Link className="logo" to="/">
-      <img src={LogoL} alt="logo"></img>
-    </Link>
+const Sidebar = () => {
+  const { t, i18n } = useTranslation()
+  const lngs = {
+    fr: { nativeName: t('french') },
+    en: { nativeName: t('english') },
+  }
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoL} alt="logo"></img>
+      </Link>
 
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="contact-link"
-        to="/contact"
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/lucas-leblanc-810457228"
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
         >
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/Dedelweiss"
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
         >
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-      </li>
-    </ul>
-  </div>
-)
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+
+      <ul>
+        <li>
+          <div class="dropdown">
+            <button class="dropbtn">{t('language')}</button>
+            <div class="dropdown-content">
+              {Object.keys(lngs).map((lng) => (
+                <button
+                  key={lng}
+                  style={{
+                    fontWeight:
+                      i18n.resolvedLanguage === lng ? 'bold' : 'normal',
+                  }}
+                  type="submit"
+                  onClick={() => i18n.changeLanguage(lng)}
+                >
+                  {lngs[lng].nativeName}
+                </button>
+              ))}
+            </div>
+          </div>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/lucas-leblanc-810457228"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Dedelweiss"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
 
 export default Sidebar
